5aa4494f2bfcb07d4c66b35787e0c62051628abe
Ivanortizc <Ivanortizc22@gmail.com>
2025-04-15 14:25:24 -0400

    Finish U3S2

--- src/main/java/com/codedifferently/lab/partA/Chickens01.java
+++ src/main/java/com/codedifferently/lab/partA/Chickens01.java
@@ -2,6 +2,30 @@ package com.codedifferently.lab.partA;
 
 public class Chickens01 {
     //Put your code here
+    public static void main(String[] args) {
+        int eggsPerChicken = 5;
+        int chickenCount = 3;
 
-    //System.out.println(totalEggs);
-}
+        int mondayEggs = chickenCount * eggsPerChicken;
+        int tuesdayEggs = (chickenCount + 1) * eggsPerChicken;
+        int wednesdayChickens = (chickenCount + 1) / 2;
+        int wednesdayEggs = wednesdayChickens * eggsPerChicken;
+        int totalEggs = mondayEggs + tuesdayEggs + wednesdayEggs;
+        System.out.println("Scenario 1 Total eggs:" + totalEggs);
+
+        eggsPerChicken = 4;
+        chickenCount = 8;
+
+        mondayEggs = chickenCount * eggsPerChicken;
+
+        tuesdayEggs = (chickenCount + 1) * eggsPerChicken;
+
+        wednesdayChickens = (chickenCount + 1) / 2;
+        wednesdayEggs = wednesdayChickens * eggsPerChicken;
+
+        totalEggs = mondayEggs + tuesdayEggs + wednesdayEggs;
+        System.out.println("Scenario 2 Total Eggs:" + totalEggs);
+    }
+    }
+
+
--- src/main/java/com/codedifferently/lab/partA/Chickens02.java
+++ src/main/java/com/codedifferently/lab/partA/Chickens02.java
@@ -3,6 +3,22 @@ package com.codedifferently.lab.partA;
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
+        int mondayEggs = 100;
+        int tuesdayEggs = 121;
+        int wednesdayEggs = 117;
+
+        int totalEggs = mondayEggs +tuesdayEggs+ wednesdayEggs;
+        double dailyAverage = totalEggs/3.0;
+
+        double monthlyAverage = dailyAverage *30;
+
+        double profitPerEggs = 0.18;
+        double monthlyProfit = monthlyAverage * profitPerEggs;
+
+        System.out.println("Daily Average: "+ dailyAverage);
+        System.out.println("Monthly Average: "+monthlyAverage);
+        System.out.println("Monthly Profit: $"+ monthlyProfit);
+
 
         //System.out.println("Daily Average:   " +dailyAverage);
         //System.out.println("Monthly Average: " +monthlyAverage);
